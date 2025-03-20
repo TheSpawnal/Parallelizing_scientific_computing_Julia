025010fd26f246999b600f75c1de9f40b508c4ac
ALJDBS <jdbs.pra@yahoo.com>
2025-03-20 04:19:04 +0100

    jacobi_method

---
+++ Jacobi_method/ex2_jacobi_method.jl
@@ -0,0 +1,8 @@
+
+# In the parallel implementation of the Jacobi method, we assumed that the method runs for 
+# a given number of iterations. In function, jacobi_with_tol at the beginning of the instructions 
+# shows how the Jacobi iterations can be stopped when the difference between iterations is small. 
+# Implement a parallel version of this function. 
+# Start with the code in Exercise 1 and add the stopping criterion implemented in jacobi_with_tol. 
+
+
